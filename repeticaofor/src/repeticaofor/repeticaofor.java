3f796fe6006992da9877e284591068e408655461
LEOCRDEV leocrdev@hotmail.com <LEOCRDEV leocrdev@hotmail.com>
2022-10-21 22:34:34 -0300

    Estrutura de repetição FOR

---
+++ repeticaofor/src/repeticaofor/repeticaofor.java
@@ -0,0 +1,14 @@
+package repeticaofor;
+
+public class repeticaofor {
+
+	/* Main é metodo auto executável em Java */
+	public static void main(String[] args) {
+
+		/* Estrutura de repetição FOR */
+
+		for (int numero = 10; numero >= 0; numero--) {
+			System.out.println("Número atual é: " + numero);
+		}
+	}
+}
